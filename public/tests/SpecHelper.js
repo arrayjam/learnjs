f17c06be31a12d1bf0304c11d4f6df255b5eb801
Yuri Feldman <yuri@tbqh.net>
2018-12-19 04:07:26 +0000

    Chapter 2

--- public/tests/SpecHelper.js
+++ public/tests/SpecHelper.js
@@ -1,28 +1,28 @@
 var fixture;
 
-function loadFixture(path) {  
+function loadFixture(path) {
   var html;
   jQuery.ajax({
-    url: '/index.html',
+    url: "/index.html",
     success: function(result) {
       html = result;
     },
     async: false
-  });          
+  });
   return $.parseHTML(html);
 }
 
 function resetFixture() {
   if (!fixture) {
-    var index = $('<div>').append(loadFixture('/index.html'));
-    var markup = index.find('div.markup');
+    var index = $("<div>").append(loadFixture("/index.html"));
+    var markup = index.find("div.markup");
     fixture = $('<div class="fixture" style="display: none">').append(markup);
-    $('body').append(fixture.clone());
+    $("body").append(fixture.clone());
   } else {
-    $('.fixture').replaceWith(fixture.clone());
+    $(".fixture").replaceWith(fixture.clone());
   }
 }
 
-beforeEach(function () {
+beforeEach(function() {
   resetFixture();
 });
